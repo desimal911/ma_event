ba7af7e7b550781b02fb9b90d2fe664bfcfbf68d
sandri4 <pletin1997@outlook.com>
2021-03-16 11:35:53 +0200

    add devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  EMAIL_VALID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :password, presence: true, length: { minimum: 8, maximum: 63 }
-  validates :email, presence: true, format: { with: EMAIL_VALID }, uniqueness: true
-
 end
