abb40b0da9663a22bd72368ed5762be9c1648743
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2021-03-27 16:37:47 +0200

    add endpoints

--- app/api/endpoints/category.rb
+++ app/api/endpoints/category.rb
@@ -1,9 +1,14 @@
 class Endpoints::Category < Grape::API
-  params do
-    requires :name, type: String
-  end
   namespace :categories do
     get do
+      categories = Category.all
+      present categories, with: Entities::Category::Base
+    end
+
+    params do
+      requires :name, type: String
+    end
+    post do
       category = Category.create(declared_params)
 
       present category, with: Entities::Category::Base
