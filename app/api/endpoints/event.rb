8343cba678381badeb82068b7d3ad3fa2c3ad27b
yaroslav.nazarenko <without.feeling.pain@gmail.com>
2021-03-27 15:57:47 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- app/api/endpoints/event.rb
+++ app/api/endpoints/event.rb
@@ -2,13 +2,15 @@ class Endpoints::Event < Grape::API
   params do
     requires :title, type: String
     requires :body, type: String
-    requires :requests, type: Array do
+    requires :category_id, type: Integer
+    optional :user_id, type: Integer
+    requires :requests_attributes, type: Array do
       requires :description, type: String
     end
   end
   namespace :events do
     post do
-      event = Event.create(declared_params)
+      event = Event.create!(declared_params)
       present event, with: Entities::Event::Base
     end
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, required: false
   belongs_to :category
 
   has_many :requests
