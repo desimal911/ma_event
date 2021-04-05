cf616dbdcfc67fa1f6c29dfe15cc33c576747a0c
“SerhiiTrokhymets” <serheyfly4410@gmail.com>
2021-04-05 11:46:16 +0300

    mob next [ci-skip] [ci skip] [skip ci]

--- app/api/entities/event.rb
+++ app/api/entities/event.rb
@@ -1,11 +1,15 @@
 module Entities::Event
   class Base < Grape::Entity
+    include Rails.application.routes.url_helpers
+
     expose :title
     expose :body
     expose :starts_at
     expose :location
     expose :category_id
-    expose :image
+    expose :image do |entity|
+      rails_blob_path(entity.image, disposition: "attachment", only_path: true)
+    end
 
     expose :requests, with: Entities::Request::Base
   end
