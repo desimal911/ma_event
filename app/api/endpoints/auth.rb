432ffd96d7e925a11bc1f64d1d409fe7cf15168a
“SerhiiTrokhymets” <serheyfly4410@gmail.com>
2021-05-08 12:45:40 +0300

    jwt fix

--- app/api/endpoints/auth.rb
+++ app/api/endpoints/auth.rb
@@ -1,21 +1,19 @@
 class Endpoints::Auth  < Grape::API
   namespace :auth do
-
-    desc 'Log in', { headers: 'Authorization' }
+    desc 'Log in'
     params do
       requires :email, type: String
       requires :password, type: String
     end
+
     post do
       user = User.authenticate(params[:email], params[:password])
       payload = { user_id: user.id }
-      token = JWT.encode payload, nil, 'HS256'
-      present(token: token)
-      user = User.find(authenticate['user_id'])
-      present user
 
-      # user = User.find(authenticate['user_id'])
-      # present(user: user)
+      token = JWT.encode payload, ENV.fetch('AUTH_SECRET'), 'HS256'
+      
+      present :token, token 
+      present :user, user
     end
   end
 end
--- app/api/helpers/shared.rb
+++ app/api/helpers/shared.rb
@@ -1,23 +1,13 @@
 module Helpers::Shared
+  attr_reader :current_user
+  
   def declared_params
     declared(params, include_missing: false).with_indifferent_access
   end
 
   def authenticate
-    @current_user = User.find(decode[:user_id])
+    token, = JWT.decode headers['Authorization'], ENV.fetch('AUTH_SECRET'), true, { algorithm: 'HS256' }  
 
+    @current_user = User.find(token['user_id'])
   end
-
-  def decode
-    JWT.decode params[:token], nil, 'HS256'
-  end
-
-  # def decode
-  #   JWT.decode(
-  #     headers['Authorization'],
-  #     Rails.application.secrets.secret_key_base,
-  #     true,
-  #     algorithm: 'HS256'
-  #   )&.first&.with_indifferent_access
-  # end
 end
